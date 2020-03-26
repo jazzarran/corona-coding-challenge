f6f9526876b2ff6b3c859cad9304c887ad39b8aa
Sarran Vidyakumar <jazzarran@gmail.com>
2020-03-26 10:53:23 -0700

    checkpoint

---
+++ src/com/jazzcorp/Testa.java
@@ -0,0 +1,17 @@
+package com.jazzcorp;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Testa {
+
+    public static void main(String[] args) {
+        HashMap<Integer,String> map = new HashMap<Integer,String>();
+        map.put(2, "Saket");
+        map.put(25, "Saurav");
+        map.put(12, "HashMap");
+
+        System.out.println();
+
+    }
+}
